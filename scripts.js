3f05f3ae85f098b84f48c87aca487ed9559b6786
Pasan Ekanayake <pasanekanayake@gmail.com>
2025-04-03 21:35:56 +0530

    commit

---
+++ scripts.js
@@ -0,0 +1,57 @@
+// Initialize Testimonies Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
+
+// Calculate the BMI
+function calculateBMI() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+
+  if (weight === "" || height === "" || weight <= 0 || height <= 0) {
+      alert("Please enter valid weight and height!");
+      return;
+  }
+
+  height = height / 100; // Convert height to meters
+  let bmi = (weight / (height * height)).toFixed(2); // BMI formula
+
+  let condition = "";
+  if (bmi < 18.5) {
+      condition = "Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+      condition = "Normal Weight";
+  } else if (bmi >= 25 && bmi < 29.9) {
+      condition = "Overweight";
+  } else {
+      condition = "Obese";
+  }
+
+  // Display results
+  document.getElementById("bmi-result").value = bmi;
+  document.getElementById("bmi-condition").value = condition;
+}
