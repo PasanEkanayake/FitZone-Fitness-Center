174090fb8ebb7d5a49c2504fc143cffeeff1a5d0
Pasan Ekanayake <pasanekanayake@gmail.com>
2025-04-04 01:06:54 +0530

    commit

---
+++ pages/register.js
@@ -0,0 +1,26 @@
+function showPassword(){
+    let eyeicon = document.getElementById("eye-icon");
+    let password = document.getElementById("password");
+    let confirmpassword = document.getElementById("confirm-password");
+
+    if(password.type == "password"){
+        password.type = "text";
+        eyeicon.src = "../public/images/eye-open.png";
+    }else{
+        password.type = "password";
+        eyeicon.src = "../public/images/eye-close.png";
+    }
+}
+
+function showConfirmPassword(){
+    let eyeicon = document.getElementById("confirm-eye-icon");
+    let confirmpassword = document.getElementById("confirm-password");
+
+    if(confirmpassword.type == "password"){
+        confirmpassword.type = "text";
+        eyeicon.src = "../public/images/eye-open.png";
+    }else{
+        confirmpassword.type = "password";
+        eyeicon.src = "../public/images/eye-close.png";
+    }
+}
