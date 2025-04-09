c417c730e3bcab2bf041ac0734286ec41ad2d345
Pasan Ekanayake <pasanekanayake@gmail.com>
2025-04-10 04:40:54 +0530

    Update reset-password.php

--- includes/reset-password.php
+++ includes/reset-password.php
@@ -38,7 +38,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Forgot Password? | FitZone Fitness Center</title>
+        <title>Set a New Password | FitZone Fitness Center</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="../public/css/header.css">
